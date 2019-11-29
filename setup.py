e0c6398614ed197626b84f626a9cfbbdd9be4dd8
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2019-11-29 23:43:35 +0530

    V020

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'saenews',         # How you named your package folder (MyLib)
   packages = ['saenews'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.2.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'required code for saenews',   # Give a short description about your library
   author = 'saenews',                   # Type in your name
   author_email = 'contact@advaitlabs.com',      # Type in your E-Mail
   url = 'https://github.com/dheerajmpai/saenews/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/dheerajmpai/saenews/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/dheerajmpai/saenews/archive/v_020.tar.gz',    # I explain this later on
   keywords = ['OPENCV', 'IMAGE PROCESSING', 'NEWS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
