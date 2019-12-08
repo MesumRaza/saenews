a8b21e6fe7c23c9c6915c3d240e87d3512c0fbd2
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2019-12-08 13:06:46 +0530

    file change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 #from setuptools import setup, find_packages
 setup(
 
-  name = 'saenews',        
+  name = 'facebook',        
   packages = ['saenews'],  
   version = '1.0.1',       
   license='MIT',        
