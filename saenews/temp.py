dceedd853105b49b36abf5f263291e1a027328aa
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-01-14 03:14:26 +0530

    file change

--- saenews/temp.py
+++ saenews/temp.py
@@ -35,5 +35,6 @@ tag_line = "(Kathopanishad)"
 input_file_orig = "SV.jpg"
 in_img = input_file_orig
 
-put_quote(input_file_orig=input_file_orig,border_dim=0.2,border_dims= (0, 0, 0,0.15), title=title,tag_line='', cord = (0.1,0.886), border_color='red', text_font = '', cap_text_font = '', cap_width=0.055, cap_cord=(0.7,0.866),focus='false')
+put_quote(input_file_orig=input_file_orig,
+border_dim=0.2,border_dims= (0, 0, 0,0.15), title=title,tag_line='', cord = (0.1,0.886), border_color='red', text_font = '', cap_text_font = '', cap_width=0.055, cap_cord=(0.7,0.866),focus='false')
 
