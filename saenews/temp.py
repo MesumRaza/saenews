6894a7987323975d1219e916029117ec61d95980
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-01-14 00:36:17 +0530

    file change

--- saenews/temp.py
+++ saenews/temp.py
@@ -1,4 +1,4 @@
 title = "Arise, Awake, stop not till the goal is reached."
 tag_line = "(Kathopanishad)"
 input_file = "SV.jpg"
-quote(title,tag_line,input_file, output_file='', cord = (0.035,0.666), font_size='', caption_width='', border_width='', logo_border='', border_color='red', text_font = '', cap_text_font = '', cap_width=0.055, cap_cord='')
+quote(title,tag_line,input_file, cord = (0.035,0.666), caption_width='', border_width='', logo_border='', border_color='red', text_font = '', cap_text_font = '', cap_width=0.055, cap_cord='')
