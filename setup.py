a0d8c50dc124b7c85d4137e551a45c2874bd0968
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-01-13 22:50:52 +0530

    file change

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   include_package_data = True,
   name = 'saenews',        
   packages = ['saenews'],  
-  version = '1.1.7',
+  version = '1.1.8',
   license='MIT',        
     # other arguments omitted
   long_description=long_description,
