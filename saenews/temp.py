b35b8c729815dfc7bcd80b1e396b42294f68182e
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-01-14 00:34:52 +0530

    file change

---
+++ saenews/temp.py
@@ -0,0 +1,3 @@
+title = "Arise, Awake, stop not till the goal is reached."
+tag_line = ""
+quote(title,tag_line,input_file, output_file='', cord = (0.035,0.666), font_size='', caption_width='', border_width='', logo_border='', border_color='red', text_font = '', cap_text_font = '', cap_width=0.055, cap_cord='')
