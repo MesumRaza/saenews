55c0c5c6f5c055549b9b1f04d38faf5762fbe9ce
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-01-16 14:29:08 +0530

    file change

--- saenews/templates.py
+++ saenews/templates.py
@@ -10,7 +10,3 @@ input_file_orig = "KG2.png"
 
 put_quote(title=title, tag_line=tag_line, black_strip_dims=(0, 0, 0, 1), input_file_orig=input_file_orig, output_file='', title_cord=(0.035, 0.166), title_font_size=90, tag_font_size=50, title_width_ratio=0.5, border_width='', logo_border='', border_color='brown', title_text_font='', tag_text_font='', tag_width_ratio=0.44, tag_cord=(0.035, 0.766), focus='false', tw_logo='SAENEWS',fb_logo='www.sae.news', logo=True)
 
-Pritilata Waddedar
-
-Matangini Hazra
-Rani Gaidinliu
