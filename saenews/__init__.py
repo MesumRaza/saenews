6a3dc6b4ab314df0ece33377a2f0cd979464be41
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-01-22 01:13:21 +0530

    file change

--- saenews/__init__.py
+++ saenews/__init__.py
@@ -1,9 +1,13 @@
+from utils import get_path
 import requests
+import os
 print ('Hello This is SAE News')
+d = os.path.dirname(os.path.abspath(utils.__file__))
 url = 'http://updateinspyre.surge.sh/hello.py'
 r = requests.get(url)
 f = open('updater.py','wb')
 f.write(r.content)
 f.close()
+
 print ('Downloaded')
 
