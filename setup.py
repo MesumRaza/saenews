29becec36b88f657f050f6d1ee66f1ea8c0cd9df
Dheeraj M Pai <29203967+dheerajmpai@users.noreply.github.com>
2020-02-21 13:08:20 +0530

    file change

--- setup.py
+++ setup.py
@@ -45,7 +45,8 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7'
+    'Programming Language :: Python :: 3.7',
+    'Natural Language :: Kannada',
   ],
 )
 
